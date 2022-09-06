ff9dc6777bcba948b1dd699259457f80a0e5c8f5
silvotape <sperelli@outlook.com>
2022-09-05 23:42:17 -0300

    Formulario php funcionando

--- mail.php
+++ mail.php
@@ -2,15 +2,15 @@
 $name = $_POST['name'];
 $email = $_POST['email'];
 $phone = $_POST['phone'];
-$call = $_POST['call'];
+//$call = $_POST['call'];
 $website = $_POST['website'];
 $priority = $_POST['priority'];
 $type = $_POST['type'];
 $message = $_POST['message'];
-$formcontent=" From: $name \n Phone: $phone \n Call Back: $call \n Website: $website \n Priority: $priority \n Type: $type \n Message: $message";
+$formcontent=" From: $name \n Phone: $phone \n Website: $website \n Priority: $priority \n Type: $type \n Message: $message";
 $recipient = "sperelli@outlook.com";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!" . " -" . "<a href='form.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
+echo "Thank You. Your information has been sent!" . " -" . "<a href='index.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
 ?>
